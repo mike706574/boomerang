9034bfc5d6c93e7243160977e5205df6dbaf665e
EAS7510 <michaeleaster@northwesternmutual.com>
2017-09-04 16:54:32 -0500

    Latest alpha.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/mike706574/boomerang"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
                  [org.clojure/spec.alpha "0.1.123"]
                  [org.clojure/data.json "0.2.6"]
                  [com.taoensso/timbre "4.10.0"]
